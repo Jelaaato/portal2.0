0f7f55c3e497636b0f0361b4286e862b08416a4b
jelaaato <erjel88@yahoo.com>
2017-04-18 13:09:54 +0800

    Update

---
+++ Portal/Controllers/ReportsController.cs
@@ -0,0 +1,206 @@
+﻿using Portal.Models.BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vereyon.Web;
+using PagedList;
+using PagedList.Mvc;
+
+namespace Portal.Controllers
+{
+    public class ReportsController : Controller
+    {
+        // GET: Reports
+
+        private OMCP omcp = new OMCP();
+
+        private int pageSize;
+        private int pageNumber;
+        private string searchstring;
+        private Guid patient_id;
+
+        [Authorize]
+        public ActionResult OMCP(string search)
+        {
+            try
+            {
+                if (search != null)
+                {
+                    if (omcp.isValidHN(search))
+                    {
+                        Session["searchstring"] = search;
+                        Session["patient_id"] = omcp.GetPatientId(search);
+                        ViewBag.DisplayPartialViews = "true";
+                        return View();
+                    }
+                    else
+                    {
+                        FlashMessage.Info("You have entered an invalid hospital number");
+                        ModelState.Clear();
+                        return View();
+                    }
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+        }
+
+        [Authorize]
+        public ActionResult OMCPAllergies(int? page, string currentfilter)
+        {
+            try
+            {
+                searchstring = Session["searchstring"].ToString();
+                patient_id = new Guid(Session["patient_id"].ToString());
+
+                if (searchstring != null && page < 1)
+                {
+                    page = 1;
+                }
+                else
+                {
+                    searchstring = currentfilter;
+                }
+
+                ViewBag.CurrentFilter = searchstring;
+                pageSize = 10;
+                pageNumber = (page ?? 1);
+
+                var allergies = omcp.GetAllergies(patient_id).ToPagedList(pageNumber, pageSize);
+                return PartialView("_allergies", allergies);
+            }
+            catch (Exception)
+            {
+                return PartialView("_allergies");
+            }
+        }
+
+        [Authorize]
+        public ActionResult OMCPDiagnosis(int? page, string currentfilter)
+        {
+            try
+            {
+                searchstring = Session["searchstring"].ToString();
+                patient_id = new Guid(Session["patient_id"].ToString());
+
+                if (searchstring != null && page < 1)
+                {
+                    page = 1;
+                }
+                else
+                {
+                    searchstring = currentfilter;
+                }
+
+                ViewBag.CurrentFilter = searchstring;
+                pageSize = 10;
+                pageNumber = (page ?? 1);
+
+                var diagnosis = omcp.GetDiagnosis(patient_id).ToPagedList(pageNumber, pageSize);
+                return PartialView("_diagnosis", diagnosis);
+            }
+            catch (Exception)
+            {
+                return PartialView("_diagnosis");
+            }
+        }
+
+        [Authorize]
+        public ActionResult OMCPHospitalizations(int? page, string currentfilter)
+        {
+            try
+            {
+                searchstring = Session["searchstring"].ToString();
+                patient_id = new Guid(Session["patient_id"].ToString());
+
+                if (searchstring != null && page < 1)
+                {
+                    page = 1;
+                }
+                else
+                {
+                    searchstring = currentfilter;
+                }
+
+                ViewBag.CurrentFilter = searchstring;
+                pageSize = 10;
+                pageNumber = (page ?? 1);
+
+                var hospitalizations = omcp.GetHospitalizations(patient_id).ToPagedList(pageNumber, pageSize);
+                return PartialView("_hospitalizations", hospitalizations);
+            }
+            catch (Exception)
+            {
+                return PartialView("_hospitalizations");
+            }
+        }
+
+        [Authorize]
+        public ActionResult OMCPMedications(int? page, string currentfilter)
+        {
+            try
+            {
+                searchstring = Session["searchstring"].ToString();
+                patient_id = new Guid(Session["patient_id"].ToString());
+
+                if (searchstring != null && page < 1)
+                {
+                    page = 1;
+                }
+                else
+                {
+                    searchstring = currentfilter;
+                }
+
+                ViewBag.CurrentFilter = searchstring;
+                pageSize = 10;
+                pageNumber = (page ?? 1);
+
+                var medications = omcp.GetMedications(patient_id).ToPagedList(pageNumber, pageSize);
+                return PartialView("_medications", medications);
+            }
+            catch (Exception)
+            {
+                return PartialView("_medications");
+            }
+        }
+
+        [Authorize]
+        public ActionResult OMCPSurgeries(int? page, string currentfilter)
+        {
+            try
+            {
+                searchstring = Session["searchstring"].ToString();
+                patient_id = new Guid(Session["patient_id"].ToString());
+
+                if (searchstring != null && page < 1)
+                {
+                    page = 1;
+                }
+                else
+                {
+                    searchstring = currentfilter;
+                }
+
+                ViewBag.CurrentFilter = searchstring;
+                pageSize = 10;
+                pageNumber = (page ?? 1);
+
+                var surgeries = omcp.GetSurgeries(patient_id).ToPagedList(pageNumber, pageSize);
+                return PartialView("_surgeries", surgeries);
+            }
+            catch (Exception)
+            {
+                return PartialView("_surgeries");
+            }
+        }
+    }
+}
---
+++ Portal/Models/BusinessLogic/OMCP.cs
@@ -0,0 +1,62 @@
+﻿using Portal.Models.Reports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal.Models.BusinessLogic
+{
+    public class OMCP
+    {
+        private reports_entities db = new reports_entities();
+
+        public bool isValidHN(string hn)
+        {
+            var isValid = db.patients.Any(a => a.hospital_number == hn);
+
+            return isValid;
+        }
+
+        public Guid GetPatientId(string hn)
+        {
+            var patientId = db.patients.Where(a => a.hospital_number == hn).Select(a => a.patient_id).First();
+
+            return patientId;
+        }
+
+        public IEnumerable<patient_allergies> GetAllergies(Guid patient_id)
+        {
+            var allergies = db.patient_allergies.Where(a => a.patient_id == patient_id).ToList().OrderBy(a => a.cause);
+
+            return allergies;
+        }
+
+        public IEnumerable<patient_diagnosis> GetDiagnosis(Guid patient_id)
+        {
+            var diagnosis = db.patient_diagnosis.Where(a => a.patient_id == patient_id).OrderByDescending(a => a.recorded_date_time);
+
+            return diagnosis;
+        }
+
+        public IEnumerable<patient_previous_hospitalizations> GetHospitalizations(Guid patient_id)
+        {
+            var hospitalizations = db.patient_previous_hospitalizations.Where(a => a.patient_id == patient_id).OrderByDescending(a => a.actual_visit_date_time);
+
+            return hospitalizations;
+        }
+
+        public IEnumerable<patient_medications> GetMedications(Guid patient_id)
+        {
+            var medications = db.patient_medications.Where(a => a.patient_id == patient_id).OrderByDescending(a => a.note_date);
+
+            return medications;
+        }
+
+        public IEnumerable<patient_previous_surgeries> GetSurgeries(Guid patient_id)
+        {
+            var surgeries = db.patient_previous_surgeries.Where(a => a.patient_id == patient_id).OrderByDescending(a => a.previous_surgeries);
+
+            return surgeries;
+        }
+    }
+}
